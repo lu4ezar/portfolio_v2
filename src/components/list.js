5af68d098787ac25f0a5809594c38be001d4c9ea
lu4ezar <misha-shisha@mail.ru>
2020-09-16 20:22:00 +0700

    feat: List component

---
+++ src/components/list.js
@@ -0,0 +1,49 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+export default function List() {
+  const data = useStaticQuery(graphql`
+    query ReposQuery {
+      github {
+        user(login: "lu4ezar") {
+          pinnedItems(first: 4) {
+            nodes {
+              ... on GitHub_Repository {
+                id
+                name
+                description
+                url
+                homepageUrl
+                primaryLanguage {
+                  color
+                  name
+                }
+                repositoryTopics(first: 10) {
+                  nodes {
+                    topic {
+                      id
+                      name
+                    }
+                    url
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+	`)
+	
+	console.log(process.env.GATSBY_GITHUB_TOKEN)
+  return (
+    <ul>
+      {data.github.user.pinnedItems.nodes.map(node => (
+        <li key={node.id}>
+          <h1>{node.name}</h1>
+          <p>{node.description}</p>
+        </li>
+      ))}
+    </ul>
+  )
+}
