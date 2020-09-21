122e55b6ced4c743ce172afaa6970ef451b40427
lu4ezar <misha-shisha@mail.ru>
2020-09-21 18:59:50 +0700

    move Arwes wrapper

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,11 +1,5 @@
 import React from "react"
-import {
-  ThemeProvider,
-  createTheme,
-  SoundsProvider,
-  createSounds,
-  Arwes,
-} from "arwes"
+import { ThemeProvider, createTheme, SoundsProvider, createSounds } from "arwes"
 
 const theme = createTheme()
 const mySounds = {
@@ -30,9 +24,7 @@ const sounds = createSounds(mySounds)
 const wrapRootElement = ({ element }) => {
   return (
     <ThemeProvider theme={theme}>
-      <Arwes animate pattern="/images/glow.png">
-        <SoundsProvider sounds={sounds}>{element}</SoundsProvider>
-      </Arwes>
+      <SoundsProvider sounds={sounds}>{element}</SoundsProvider>
     </ThemeProvider>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
+import { Arwes, Content, Header, Words } from "arwes"
 import React from "react"
 import List from "../components/list"
 
 export default function Home({ data }) {
   return (
-    <>
-      <h1>My Portfolio</h1>
-      <List />
-    </>
+    <Arwes
+      animate
+      pattern="/images/glow.png"
+      background="/images/background.jpg"
+    >
+      <Content style={{ margin: 20 }}>
+        <Header>
+          <h1>
+            <Words animate>Projects</Words>
+          </h1>
+        </Header>
+        <List />
+      </Content>
+    </Arwes>
   )
 }
